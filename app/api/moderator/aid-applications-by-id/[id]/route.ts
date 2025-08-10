16425ea6fa4a2c2da6dba497190ca69cf518697b
fardeenahmed2002 <myworktime2022@gamil.com>
2025-08-10 13:28:33 +0600

    refactor

--- app/api/moderator/aid-applications-by-id/[id]/route.ts
+++ app/api/moderator/aid-applications-by-id/[id]/route.ts
@@ -1,15 +1,21 @@
-import { applicationsById } from "@/controller/ModeratorController"
-import { NextResponse } from "next/server"
+import { applicationsById } from "@/controller/ModeratorController";
+import { NextResponse } from "next/server";
 
-export const GET = async (req: Request, { params }: { params: { id: string } }) => {
+export async function GET(
+    request: Request,
+    context: { params: { id: string } }
+) {
     try {
-        const { id } = params
-        return await applicationsById(id)
+        const { id } = context.params;
+        return await applicationsById(id);
     } catch (error) {
-        return NextResponse.json({
-            success: false,
-            message: `server error`,
-            error: (error as Error).message
-        }, { status: 500 })
+        return NextResponse.json(
+            {
+                success: false,
+                message: "server error",
+                error: (error as Error).message,
+            },
+            { status: 500 }
+        );
     }
 }
