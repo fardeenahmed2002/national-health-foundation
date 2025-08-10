8d86d0195f068a77358828f0b66b46caeb509f42
fardeenahmed2002 <myworktime2022@gamil.com>
2025-08-10 14:27:13 +0600

    deploy

--- app/api/moderator/aid-applications-by-id/[id]/route.ts
+++ app/api/moderator/aid-applications-by-id/[id]/route.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextResponse } from "next/server";
 import { applicationsById } from "@/controller/ModeratorController";
+import { NextResponse } from "next/server";
 
-export async function GET(request: Request, context: any) {
-  try {
-    const { id } = context.params;
-    return await applicationsById(id);
-  } catch (error) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: "server error",
-        error: (error as Error).message,
-      },
-      { status: 500 }
-    );
-  }
+export async function GET(request: Request, context: any): Promise<NextResponse> {
+    try {
+        const { id } = context.params;
+        return await applicationsById(id);
+    } catch (error) {
+        return NextResponse.json(
+            {
+                success: false,
+                message: "server error",
+                error: (error as Error).message,
+            },
+            { status: 500 }
+        );
+    }
 }
