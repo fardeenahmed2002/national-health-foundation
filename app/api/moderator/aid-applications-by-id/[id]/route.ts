f7a4fda8ce82ba751705b4727e4ebefd97faa8be
fardeenahmed2002 <myworktime2022@gamil.com>
2025-08-10 13:58:11 +0600

    route

--- app/api/moderator/aid-applications-by-id/[id]/route.ts
+++ app/api/moderator/aid-applications-by-id/[id]/route.ts
@@ -1,18 +1,27 @@
-import { applicationsById } from '@/controller/ModeratorController'
 import { NextResponse } from 'next/server'
+import { applicationsById } from '@/controller/ModeratorController'
 
-export async function GET(request: Request, context: any): Promise<NextResponse> {
-    try {
-        const { id } = context.params
-        return await applicationsById(id)
-    } catch (error) {
-        return NextResponse.json(
-            {
-                success: false,
-                message: 'server error',
-                error: (error as Error).message,
-            },
-            { status: 500 }
-        )
-    }
+type Params = {
+  params: {
+    id: string
+  }
+}
+
+export async function GET(
+  request: Request,
+  context: Params
+) {
+  try {
+    const { id } = context.params
+    return await applicationsById(id)
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'server error',
+        error: (error as Error).message,
+      },
+      { status: 500 }
+    )
+  }
 }
